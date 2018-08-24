483dc90b2cc6a012fe03739bdef478ef33980609
Song Chen <csongbj@cn.ibm.com>
2018-08-24 17:22:41 +0800

    git update

--- Src/SyncToDashDB.py
+++ Src/SyncToDashDB.py
@@ -1,12 +1,13 @@
-import GetPara as getpara
-import DB2CLI as db2
-import FileTransfer as filedtl
-import LiftCLI as lift
+import Src.GetPara as getpara
+import Src.DB2CLI as db2
+import Src.FileTransfer as filedtl
+import Src.LiftCLI as lift
 from Tool.Log import *
 
 
 def put_file_to_lift_server():
     para = getpara.get_dsjob_para()
+    print(para)
     exp_file = db2.exeExport(para['TABLE'], '.', para['DB2ENV'], db2.export(**para))
     gz_file = filedtl.compress(exp_file)
     conn = filedtl.ssh_conn('pdydaldal1302.sl.bluecloud.ibm.com', 'dswusr1', 'Get1ndsw')
