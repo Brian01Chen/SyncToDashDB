30da52b2444d718f765fa33c97c8f0aced940fa3
Song Chen <csongbj@cn.ibm.com>
2018-08-27 19:56:27 +0800

    delete files

--- DWDM2.APPL_DATA_RECENCY.sql
+++
@@ -1,2 +0,0 @@
-connect to DSSRUG user csongbj using Feilong03 @
-export to DWDM2.APPL_DATA_RECENCY.csv of del select * from DWDM2.APPL_DATA_RECENCY @
--- dwdm1.SW_SBSCRPTN_ID_ACQSTN_LNK.sql
+++
@@ -1,2 +0,0 @@
-connect to DSSRUG user csongbj using Feilong03 @
-export to dwdm1.SW_SBSCRPTN_ID_ACQSTN_LNK.csv of del select * from dwdm1.SW_SBSCRPTN_ID_ACQSTN_LNK @
